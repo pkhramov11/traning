42ea652d58da949965a1555a062d6501719036f2
pavel_xramov <pavel_xramov@mail.ru>
2023-05-04 19:22:36 +0300

    !

--- src/Main2.java
+++ src/Main2.java
@@ -72,6 +72,13 @@ public class Main2 {
             }
         }
         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 // check
     }
 }
